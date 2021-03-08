ddcc0f26813c3bb6b2d3f2c5a7f4d2e6d88a9b56
Aditya Singh <201394@juitsolan.in>
2021-03-08 13:51:55 +0530

    initial commit

---
+++ src/main/java/ttviewer/SecondaryController.java
@@ -0,0 +1,12 @@
+package ttviewer;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
