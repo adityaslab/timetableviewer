15b6af85344d5cc979087afb5a7b0ae195a001b0
Aditya Singh <201394@juitsolan.in>
2021-03-10 18:47:21 +0530

    Functionality to find the cell with specific day

--- src/main/java/ttviewer/reader/SpreadsheetReader.java
+++ src/main/java/ttviewer/reader/SpreadsheetReader.java
@@ -9,46 +9,67 @@ import org.apache.poi.ss.usermodel.CellType;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SpreadsheetReader {
+    public static String[] day={"Mon","Tue","Wed","Fri","Sat","Sun"};
     /*
-    This function reads the .xls spreadsheet file
+    This function finds the cell of day asked and cell of subsequent day and return them in an ArrayList
+    required day is passed as an integer
      */
+    public static ArrayList<HSSFCell> findDay(HSSFSheet sheet,String[] day,int i){
+        ArrayList<HSSFCell> ar=new ArrayList<HSSFCell>();
+        int totalrow=sheet.getPhysicalNumberOfRows();
+        int j=0;
+        for(;j<totalrow;j++){
+            HSSFRow row=sheet.getRow(j);
+            if(row==null)
+                continue;
+            HSSFCell cell=row.getCell(0);
+            String cellval=cell.getStringCellValue();
+            if(day[i].equals(cellval)){
+                System.out.println("Its tuesday today!!!");
+                System.out.println(cell.getStringCellValue());
+                ar.add(cell);
+                break;
+
+            }
+        }
+
+        for(;j<totalrow;j++) {
+            HSSFRow row = sheet.getRow(j);
+            if (row == null)
+                continue;
+            HSSFCell cell = row.getCell(0);
+            String cellval = cell.getStringCellValue();
+            if (day[i + 1].equals(cellval)) {
+                System.out.println(cell.getStringCellValue());
+                ar.add(cell);
+                break;
+
+            }
+        }
+        return ar;
+    }
+
+
     public static void main(String[] args){
         //loc here contains location of the timetable file
         //Sting loc=args[0]
         String loc="/home/thinkpad/Downloads/t.xls";
         String sem="BTECH 2 SEM";
-        String day="Mon";
+
         try (HSSFWorkbook wb=File2Workbook.readF(loc)){
             int sheetsno=wb.getNumberOfSheets();
             int i=0;
             for(;i<sheetsno;i++){
                 String nm=wb.getSheetName(i);
                 if(sem.equals(nm)){
-                    System.out.println("this works");
                     break;
                 }
-
             }
             HSSFSheet sheet= wb.getSheetAt(i);
-            int totalrow=sheet.getPhysicalNumberOfRows();
-            for(int j=0;j<totalrow;j++){
-                HSSFRow row=sheet.getRow(j);
-                if(row==null)
-                    continue;
-                HSSFCell cell=row.getCell(0);
-                String cellval=cell.getStringCellValue();
-
-                if(day.equals(cellval)){
-                    System.out.println("Its tuesday today!!!");
-                }
-
-                int lastcelln=row.getLastCellNum();
-
-            }
-
-
+            ArrayList<HSSFCell> dayrow=findDay(sheet,day,1);
 
         } catch (IOException e) {
             e.printStackTrace();
